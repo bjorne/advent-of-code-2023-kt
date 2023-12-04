4bc5f9f4e5df319bc878093b0e2ab5d8d811b253
Björn Lansing <bjornr@spotify.com>
2023-12-04 22:58:24 +0100

    Point to own file

---
+++ src/main/kotlin/adventofcode/shared/Point.kt
@@ -0,0 +1,57 @@
+package adventofcode.shared
+
+import kotlin.math.absoluteValue
+
+data class Point(val x: Int, val y: Int) {
+    val neigbors = sequence {
+        yield(Point(x - 1, y - 1))
+        yield(Point(x, y - 1))
+        yield(Point(x + 1, y - 1))
+        yield(Point(x - 1, y))
+        yield(Point(x + 1, y))
+        yield(Point(x - 1, y + 1))
+        yield(Point(x, y + 1))
+        yield(Point(x + 1, y + 1))
+    }
+
+    operator fun plus(other: Point) = Point(x + other.x, y + other.y)
+    operator fun minus(other: Point) = Point(x - other.x, y - other.y)
+
+    operator fun rangeTo(other: Point) = sequence {
+        var ix = x
+        var iy = y
+        val xDiffTotal = (other.x - x).absoluteValue
+        val yDiffTotal = (other.y - y).absoluteValue
+        yield(Point(ix, iy))
+        while (ix != other.x || iy != other.y) {
+            val xDiff = (other.x - ix).absoluteValue
+            val yDiff = (other.y - iy).absoluteValue
+            if (xDiffTotal == 0 || xDiff * yDiffTotal < yDiff * xDiffTotal) {
+                iy += if (iy < other.y) 1 else -1
+            } else {
+                ix += if (ix < other.x) 1 else -1
+            }
+            yield(Point(ix, iy))
+        }
+    }
+
+    // TODO: Why doesn't operator fun work?
+    fun rangeUntil(other: Point) = sequence {
+        val xRange = if (x < other.x) x until other.x else other.x until x
+        val yRange = if (y < other.y) y until other.y else other.y until y
+        for (x in xRange) {
+            for (y in yRange) {
+                yield(Point(x, y))
+            }
+        }
+    }
+
+    companion object Constants {
+        val origin = Point(0, 0)
+        val up = Point(0, -1)
+        val down = Point(0, 1)
+        val left = Point(-1, 0)
+        val right = Point(1, 0)
+    }
+}
+
--- src/test/kotlin/adventofcode/shared/PointTest.kt
+++ src/test/kotlin/adventofcode/shared/PointTest.kt
@@ -8,14 +8,34 @@ internal class PointTest {
 
     @Test
     fun plus() {
+        assertEquals(Point(4, 6), Point(1, 2) + Point(3, 4))
     }
 
     @Test
     fun minus() {
+        assertEquals(Point(-2, -2), Point(1, 2) - Point(3, 4))
+
     }
 
     @Test
     fun rangeTo() {
+        assertEquals(listOf(
+            Point(1, 2),
+            Point(1, 3),
+        ), (Point(1, 2) .. Point(1, 3)).toList())
+        assertEquals(listOf(
+            Point(1, 2),
+            Point(2, 2),
+            Point(2, 3),
+            ), (Point(1, 2) .. Point(2, 3)).toList())
+        assertEquals(listOf(
+            Point(1, 2),
+            Point(2, 2),
+            Point(2, 3),
+            Point(3, 3),
+            Point(4, 3),
+        ), (Point(1, 2) .. Point(4, 3)).toList())
+
     }
 
     @Test
