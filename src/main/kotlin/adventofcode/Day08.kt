f722b4f5f746d88cd3a6060ff0487386d3b0e6bb
Björn Lansing <bjornr@spotify.com>
2023-12-08 14:04:59 +0100

    Day 08 semi-brute force

---
+++ src/main/kotlin/adventofcode/Day08.kt
@@ -0,0 +1,67 @@
+package adventofcode
+
+object Day08 {
+    fun a(input: String): Int {
+        val (turns, map) = parseInput(input)
+        val (count, _) = findLength(turns, map)
+        return count.toInt()
+    }
+
+    fun findLength(
+        turns: String,
+        map: Map<String, Pair<String, String>>,
+        start: String = "AAA",
+        end: Regex = "ZZZ".toRegex(),
+        startI: Long = 0
+    ): Pair<Long, String> {
+        var i = startI
+        var next = start
+        while (true) {
+            val turn = turns[(i++ % turns.length.toLong()).toInt()]
+            val (left, right) = map[next]!!
+            when (turn) {
+                'L' -> next = left
+                'R' -> next = right
+            }
+            if (end.matches(next)) {
+                return (i - startI) to next
+            }
+        }
+    }
+
+    private fun parseInput(input: String): Pair<String, Map<String, Pair<String, String>>> {
+        val (turns, mapStr) = input.split("\n\n")
+        val map = mapStr.lines().map { line ->
+            val (from, left, right) = "([A-Z0-9]+) = \\(([A-Z0-9]+), ([A-Z0-9]+)\\)".toRegex()
+                .matchEntire(line)!!.destructured
+            from to (left to right)
+        }.toMap()
+        return turns to map
+    }
+
+    private val endRegex = "..Z".toRegex()
+
+    fun b(input: String): Long {
+        val (turns, map) = parseInput(input)
+
+        val cache = mutableMapOf<Pair<Long, String>, Pair<Long, String>>()
+        val (iter, pos) = map.keys.filter { "..A".toRegex().matches(it) }
+            .map { findLength(turns, map, it, "..Z".toRegex(), 0) }.unzip()
+        val iterations = iter.toMutableList()
+        val positions = pos.toMutableList()
+
+        while (iterations.distinct().size > 1) {
+            val maxIterations = iterations.max()!!
+            for (i in positions.indices) {
+                while (iterations[i] < maxIterations) {
+                    val (length, pos) = cache.getOrPut((iterations[i] % turns.length) to positions[i]) {
+                        findLength(turns, map, positions[i], endRegex, iterations[i])
+                    }
+                    iterations[i] = iterations[i] + length
+                    positions[i] = pos
+                }
+            }
+        }
+        return iterations.first()
+    }
+}
---
+++ src/test/kotlin/adventofcode/Day08Test.kt
@@ -0,0 +1,58 @@
+package adventofcode.shared.adventofcode
+
+import adventofcode.Day08
+import adventofcode.shared.getInput
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day08Test {
+
+    val input = """RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)""".trimMargin()
+
+    val input2 = """LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)""".trimMargin()
+
+    val input3 = """LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)""".trimMargin()
+
+    @Test
+    fun a() {
+        assertEquals(2, Day08.a(input))
+        assertEquals(6, Day08.a(input2))
+    }
+
+    @Test
+    fun b() {
+        assertEquals(6, Day08.b(input3))
+    }
+
+    @Test
+    fun aFull() {
+        assertEquals(14257, Day08.a(getInput(8)))
+    }
+
+    @Test
+    fun bFull() {
+        // assertEquals(16187743689077, Day08.b(getInput(8)))
+    }
+
+}
